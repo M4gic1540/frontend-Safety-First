29befb1e3ffa7cce1d11000beb82bcc18780bfff
tomas <t.gonzalezb@duocuc.cl>
2025-02-03 01:49:53 -0300

    update for production

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    
-  ],
-})
+  plugins: [react()],
+  preview: {
+    allowedHosts: ['frontend-safety-first-production.up.railway.app'],
+    host: true  // Esto permite escuchar en todas las interfaces (0.0.0.0)
+  }
+});
